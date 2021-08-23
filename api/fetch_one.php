92746a7aa1a36eebfbc9478c0bc32ab1c0f445bc
jacquarg <guillaume.jacquart@hoodbrains.com>
2021-08-23 20:00:06 +0200

    Parrallelize requests to twitter.

--- api/fetch_one.php
+++ api/fetch_one.php
@@ -1,19 +1,27 @@
 <?php
+
+    require __DIR__ . '/vendor/autoload.php';
+
+    use Amp\Promise;
+    use function Amp\ParallelFunctions\parallelMap;
+
     function fetchOne() {
-        $token = "AAAAAAAAAAAAAAAAAAAAAMu3HAEAAAAAkZ6m%2BmuccXSqyjDWOjLHpYdXCJA%3D5HPJ3XHGVwltP2LA1DLkW2LH9PS2V90nxkHzpU8Mb4ijNnJ8tx";
-
-        $options = array('http' => array(
-            'method'  => 'GET',
-            'header' => 'Authorization: Bearer '.$token
-        ));
-        $context  = stream_context_create($options);
-
-
+        
         $words = ["je", "t'ai", "donne", "de", "moi", "tu", "as", "aussi", "pris", "beaucoup", "ca",
         //"tu",
             "pourras", "le", "garder"];
+
         $res = array();
-        foreach($words as $word) {
+        $resList = Promise\wait(parallelMap($words, function($word) {    
+        
+            $token = "AAAAAAAAAAAAAAAAAAAAAMu3HAEAAAAAkZ6m%2BmuccXSqyjDWOjLHpYdXCJA%3D5HPJ3XHGVwltP2LA1DLkW2LH9PS2V90nxkHzpU8Mb4ijNnJ8tx";
+
+            $options = array('http' => array(
+                'method'  => 'GET',
+                'header' => 'Authorization: Bearer '.$token
+            ));
+            $context  = stream_context_create($options);
+
             $url = 'https://api.twitter.com/2/tweets/search/recent?expansions=author_id&query="'.$word.'"';
 
             $raw = file_get_contents($url, false, $context);
@@ -40,9 +48,14 @@
 
             $userIndex = array_search($tweet->author_id, array_column($response->includes->users, 'id'));
             $resTweet["username"] = ($userIndex !== false ? $response->includes->users[$userIndex]->username : null);
+            return $resTweet;
+        }));
 
-            $res[$word] = $resTweet;
+        
+        foreach($words as $index=>$word) {
+            $res[$word] = $resList[$index];
         }
+
         $res["timestamp"] = date("c");
         // return addslashes(json_encode($res, JSON_UNESCAPED_UNICODE));
         // return json_encode($res, JSON_UNESCAPED_UNICODE);
